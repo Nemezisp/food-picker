22f173b8effa7faefc15a4e81f8a94b42191b4d3
Nemezisp <p.jarmula5@gmail.com>
2022-04-25 18:13:35 +0200

    fix deployment issue

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ class MyDocument extends Document {
             </body>
             <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"
             integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=="
-            crossorigin=""></script>
+            crossOrigin=""></script>
         </Html>
     }
 }
